13899d3eb3efc68750750c02933e4270a6c4f819
bagasgolan9 <ksatrialangit63@gmail.com>
2025-01-28 14:43:35 +0700

    major change

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_application_1/dokter_page.dart';
 
-Future<void> main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Supabase.initialize(
-    url: 'https://qpbbxtgxsvzajmfnryhg.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFwYmJ4dGd4c3Z6YWptZm5yeWhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYyNjYwMDgsImV4cCI6MjA1MTg0MjAwOH0.euZzImoloGBnafAKGLG9F4rLvMkUJhXxriZjAnqtOLU',
-  );
-  runApp(const MainApp());
+void main() async {
+  
+  runApp(MyApp());
 }
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Supabase ListView Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: MyHomePage(),
     );
   }
 }
+
+
